5ee5e8fbebb9afa261984bddaeaa100b6a6e44cc
fitthejob <andrew@fitthejob.com>
2026-06-06 15:25:13 -0400

    narrowed permission to satisfy checkov scans

--- connect-pbx/modules/l0-account-baseline/iam.tf
+++ connect-pbx/modules/l0-account-baseline/iam.tf
@@ -1,3 +1,58 @@
+locals {
+  project_bucket_arns = [
+    "arn:aws:s3:::${var.state_bucket}",
+    "arn:aws:s3:::${var.org_name}-*",
+  ]
+
+  project_object_arns = [
+    "arn:aws:s3:::${var.state_bucket}/*",
+    "arn:aws:s3:::${var.org_name}-*/*",
+  ]
+
+  project_dynamodb_arns = [
+    "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.org_name}-*",
+    "arn:aws:dynamodb:${var.aws_region}:${data.aws_caller_identity.current.account_id}:table/${var.org_name}-*/index/*",
+  ]
+
+  project_topic_arns = [
+    "arn:aws:sns:${var.aws_region}:${data.aws_caller_identity.current.account_id}:${var.org_name}-*",
+  ]
+
+  project_event_bus_arns = compact(concat(
+    ["arn:aws:events:${var.aws_region}:${data.aws_caller_identity.current.account_id}:event-bus/default"],
+    var.deployment_profile.shared_bus_arn != "" ? [var.deployment_profile.shared_bus_arn] : [],
+  ))
+
+  project_connect_manage_arns = [
+    "arn:aws:connect:${var.aws_region}:${data.aws_caller_identity.current.account_id}:instance/*",
+    "arn:aws:connect:${var.aws_region}:${data.aws_caller_identity.current.account_id}:instance/*/*",
+    "arn:aws:connect:${var.aws_region}:${data.aws_caller_identity.current.account_id}:phone-number/*",
+  ]
+
+  project_log_group_arns = [
+    "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.org_name}-*",
+    "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${var.org_name}-*:*",
+    "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/cloudtrail/${var.org_name}-*",
+    "arn:aws:logs:${var.aws_region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/cloudtrail/${var.org_name}-*:*",
+  ]
+
+  project_kms_key_arns = [
+    "arn:aws:kms:${var.aws_region}:${data.aws_caller_identity.current.account_id}:key/*",
+  ]
+
+  project_ssm_parameter_arns = [
+    "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/${var.org_name}/*",
+  ]
+
+  project_secret_arns = [
+    "arn:aws:secretsmanager:${var.aws_region}:${data.aws_caller_identity.current.account_id}:secret:${var.org_name}*",
+  ]
+
+  project_ses_identity_arns = [
+    "arn:aws:ses:${var.aws_region}:${data.aws_caller_identity.current.account_id}:identity/*",
+  ]
+}
+
 resource "aws_iam_policy" "platform_boundary" {
   name        = "${var.org_name}-platform-boundary"
   description = "Permission boundary applied to all platform Lambda and service roles."
@@ -6,44 +61,157 @@ resource "aws_iam_policy" "platform_boundary" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid    = "AllowPlatformServices"
+        Sid    = "AllowProjectS3Objects"
         Effect = "Allow"
         Action = [
-          "connect:*",
-          "lambda:InvokeFunction",
           "s3:GetObject",
           "s3:PutObject",
           "s3:DeleteObject",
+        ]
+        Resource = local.project_object_arns
+      },
+      {
+        Sid    = "AllowProjectS3Buckets"
+        Effect = "Allow"
+        Action = [
           "s3:ListBucket",
           "s3:GetBucketAcl",
+        ]
+        Resource = local.project_bucket_arns
+      },
+      {
+        Sid    = "AllowProjectDynamoDB"
+        Effect = "Allow"
+        Action = [
           "dynamodb:GetItem",
           "dynamodb:PutItem",
           "dynamodb:UpdateItem",
           "dynamodb:DeleteItem",
           "dynamodb:Query",
           "dynamodb:Scan",
-                            "events:PutEvents",
-                            "cloudtrail:LookupEvents",
-                            "sns:Publish",
-          "sqs:SendMessage",
-          "sqs:ReceiveMessage",
-          "sqs:DeleteMessage",
-          "transcribe:StartTranscriptionJob",
-          "transcribe:GetTranscriptionJob",
-          "lex:RecognizeText",
+        ]
+        Resource = local.project_dynamodb_arns
+      },
+      {
+        Sid    = "AllowProjectEventBridge"
+        Effect = "Allow"
+        Action = [
+          "events:PutEvents",
+        ]
+        Resource = local.project_event_bus_arns
+      },
+      {
+        Sid    = "AllowProjectSNS"
+        Effect = "Allow"
+        Action = [
+          "sns:Publish",
+        ]
+        Resource = local.project_topic_arns
+      },
+      {
+        Sid    = "AllowConnectAssociations"
+        Effect = "Allow"
+        Action = [
+          "connect:AssociatePhoneNumberContactFlow",
+          "connect:DisassociatePhoneNumberContactFlow",
+        ]
+        Resource = local.project_connect_manage_arns
+      },
+      {
+        Sid    = "AllowConnectReadOnlyListCalls"
+        Effect = "Allow"
+        Action = [
+          "connect:ListPhoneNumbersV2",
+          "connect:ListUsers",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowSESOutboundMail"
+        Effect = "Allow"
+        Action = [
           "ses:SendEmail",
-          "cloudwatch:PutMetricData",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ]
+        Resource = local.project_ses_identity_arns
+      },
+      {
+        Sid    = "AllowProjectLogs"
+        Effect = "Allow"
+        Action = [
           "logs:CreateLogGroup",
           "logs:CreateLogStream",
           "logs:PutLogEvents",
-          "xray:PutTraceSegments",
-          "xray:PutTelemetryRecords",
+        ]
+        Resource = local.project_log_group_arns
+      },
+      {
+        Sid    = "AllowProjectKMSUsage"
+        Effect = "Allow"
+        Action = [
           "kms:Decrypt",
           "kms:GenerateDataKey",
           "kms:DescribeKey",
+        ]
+        Resource = local.project_kms_key_arns
+        Condition = {
+          "ForAnyValue:StringLike" = {
+            "kms:ResourceAliases" = "alias/${var.org_name}-*"
+          }
+        }
+      },
+      {
+        Sid    = "AllowProjectSSMParameters"
+        Effect = "Allow"
+        Action = [
           "ssm:GetParameter",
           "ssm:GetParameters",
-          "secretsmanager:GetSecretValue"
+        ]
+        Resource = local.project_ssm_parameter_arns
+      },
+      {
+        Sid    = "AllowProjectSecrets"
+        Effect = "Allow"
+        Action = [
+          "secretsmanager:GetSecretValue",
+        ]
+        Resource = local.project_secret_arns
+      },
+      {
+        Sid    = "AllowCloudWatchMetricsForApprovedNamespaces"
+        Effect = "Allow"
+        Action = [
+          "cloudwatch:PutMetricData",
+        ]
+        Resource = "*"
+        Condition = {
+          StringLike = {
+            "cloudwatch:namespace" = [
+              "ConnectPBX/*",
+              "${var.org_name}/*",
+            ]
+          }
+        }
+      },
+      {
+        Sid    = "AllowAuditReadOnlyQueries"
+        Effect = "Allow"
+        Action = [
+          "cloudtrail:LookupEvents",
+          "config:DescribeComplianceByConfigRule",
+          "config:DescribeConfigurationRecorderStatus",
+          "config:GetDiscoveredResourceCounts",
+          "securityhub:GetFindings",
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowXRayTelemetry"
+        Effect = "Allow"
+        Action = [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords",
         ]
         Resource = "*"
       },
@@ -62,7 +230,7 @@ resource "aws_iam_policy" "platform_boundary" {
           "iam:PassRole",
           "iam:UpdateAssumeRolePolicy",
           "iam:DeleteRolePermissionsBoundary",
-          "iam:DeleteUserPermissionsBoundary"
+          "iam:DeleteUserPermissionsBoundary",
         ]
         Resource = "*"
       },
@@ -72,7 +240,7 @@ resource "aws_iam_policy" "platform_boundary" {
         Action = [
           "kms:ScheduleKeyDeletion",
           "kms:DeleteAlias",
-          "kms:DisableKey"
+          "kms:DisableKey",
         ]
         Resource = "*"
       },
@@ -84,7 +252,7 @@ resource "aws_iam_policy" "platform_boundary" {
           "sts:*",
           "cloudwatch:PutMetricData",
           "s3:GetBucketLocation",
-          "s3:ListAllMyBuckets"
+          "s3:ListAllMyBuckets",
         ]
         Resource = "*"
         Condition = {
